402f2e060116d95df6ffbecb5cec869faf452885
hiro9108 <hiroshi.8.egawa@gmail.com>
2020-09-24 10:22:04 -0700

    first-commit

--- halfSorted.js
+++ halfSorted.js
@@ -10,7 +10,15 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45]
 
 
 const halfSorted = function(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    
+    const firstHalfArr = arr.splice(0, Math.floor((left + right) / 2 ));
 
+    firstHalfArr.sort((a, b) => a - b);
+    arr.sort((a, b) => b - a);
+
+    return firstHalfArr.concat(arr);
 };
 
 
--- insertionSort.js
+++ insertionSort.js
@@ -8,6 +8,24 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45];
 
 
 const insertionSort = function(arr) {
+    /*
+     This logic assumes that first array's index is base number at first.(It is called 'sorted'?)
+     So, this first loop start i=1.
+    */
+    for (let i = 1; i < arr.length; i++) { // first loop.
+        let temp = arr[i]; // Escape the number because this number insert later.
+        
+        let j; // declare j variable here.(Using 'var' is possible, but I don't want to use it)
+        for (j = i-1; j >= 0; j--) { // Compare array element value from right to left.
+            if (arr[j] > temp) { // if 'arr[j]' is bigger than 'temp', 'arr[j]' slide to right.
+                arr[j+1] = arr[j];
+            } else {
+                break; // if 'arr[j]' is not bigger than 'temp', break from second loop. And, insert temp variable in next step.
+            }
+        };
+        arr[j+1] = temp; // Insert temp variable.
+    };
+    return arr; // return sorted array.
 
 };
 
