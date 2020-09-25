54be21eb7acc9d5db2dbbf5a7f19d7e86917aa0a
hiro9108 <hiroshi.8.egawa@gmail.com>
2020-09-25 08:32:41 -0700

    fix refactoring

--- halfSorted.js
+++ halfSorted.js
@@ -1,8 +1,10 @@
 // Write a function that takes in an array, and sorts the first
-// half of the array in ascending order, and the second half 
+// half of the array in ascending order, and the second half
 // of the array in descending order
 
-// you may use methods for this. 
+// you may use methods for this.
+
+// reactoring
 
 let arr1 = [4, 78, 54, 34, 2, 8, 45, 77, 99, 23, 567, 6, 7]
 
@@ -10,7 +12,7 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45]
 
 
 const halfSorted = function(arr) {
-    
+
     const mid = Math.floor((arr.length - 1) / 2);
 
     const [firstHalfArr, secondHalfArr] = [arr.slice(0, mid), arr.slice(mid)];
